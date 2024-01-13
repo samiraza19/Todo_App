cbe4047703f6338fb8adeb0be997571eb0254a96
=SamiRaza <=samirazagb@gmail.com>
2024-01-13 09:58:23 +0500

    Work Complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'font-awesome/css/font-awesome.min.css'
+import TodoApp from './Components/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
